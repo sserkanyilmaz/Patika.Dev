e000478b78e68add9e922dde23016286794a8873
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-04-23 16:54:20 +0300

    app.patika.dev/serkanyilmazz

---
+++ Java101/src/EbokEkok.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class EbokEkok {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sayi1,sayi2,kontrol,enBuyukBolen=0,enKucukBolen=0;
+        System.out.println("1. sayiyi giriniz : ");
+        sayi1= scanner.nextInt();
+        System.out.println("2. sayiyi giriniz : ");
+        sayi2=scanner.nextInt();
+        int i=1,j;
+        while(i<=sayi1)
+        {
+            if(sayi1%i==0)
+            {
+                j=1;
+                while(j<=sayi2)
+                {
+                    if(sayi2%j==0)
+                    {
+                        if(j==i)
+                            enBuyukBolen=i;
+                    }
+                    j++;
+                }
+            }
+            i++;
+        }
+        System.out.println(enBuyukBolen);
+        enKucukBolen=(sayi2*sayi1)/enBuyukBolen;
+        System.out.println(enKucukBolen);
+    }
+}
