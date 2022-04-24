dda30f042cf193f77824e0364ea2c9b5f1e43eb8
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-04-24 22:23:08 +0300

    app.patika.dev/serkanyilmazz

--- Java101/src/DongulerIleBolunme.java
+++ Java101/src/DongulerIleBolunme.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 public class DongulerIleBolunme {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        int sayi1;
+        int sayi1,j=0;
+        double toplam=0;
         sayi1=scanner.nextInt();
         for (int i=0;i<=sayi1;i++)
         {
-            if(i%12==0)
+            if(i%12==0){
                 System.out.println(i);
+                toplam+=i;
+                j++;
+            }
         }
+        System.out.println("Ortalama : "+(toplam/j));
     }
 }
