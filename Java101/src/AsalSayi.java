041ccde44b44dd1849485113187bb1a7f74728d8
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-04-29 08:37:18 +0300

    app.patika.dev/serkanyilmazz

--- Java101/src/AsalSayi.java
+++ Java101/src/AsalSayi.java
@@ -1,15 +1,15 @@
 public class AsalSayi {
     public static void main(String[] args) {
-        for (int i = 2;i<=100;i++)
-        {
-            if(i==2)
-                System.out.print(i+" ");
+        int kontrol=0;
+        for (int i = 2; i < 100; i++) {
+            kontrol=0;
             for (int j = 2; j < i; j++) {
-                if (i % j != 0) {
-                    System.out.print(i + " ");
+                if (i % j == 0) {
+                    kontrol++;
                 }
-                break;
             }
+            if(kontrol==0)
+                System.out.println(i + " ");
         }
     }
 }
