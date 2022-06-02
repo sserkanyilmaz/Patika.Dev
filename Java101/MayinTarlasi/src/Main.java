ee52cb761b1f52dbbcba9b9c8c4c9bf796f86e69
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-06-02 17:59:41 +0300

    https://app.patika.dev/serkanyilmazz

--- Java101/MayinTarlasi/src/Main.java
+++ Java101/MayinTarlasi/src/Main.java
@@ -1,10 +1,6 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         MineSweeper mine = new MineSweeper();
         mine.run();
-
     }
 }
