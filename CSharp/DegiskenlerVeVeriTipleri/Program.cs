f55fe1f2ada0976a9509d72c07745b63fec75e13
sserkanyilmaz <92798864+sserkanyilmaz@users.noreply.github.com>
2022-06-14 13:22:29 +0300

    https://app.patika.dev/serkanyilmazz

--- CSharp/DegiskenlerVeVeriTipleri/Program.cs
+++ CSharp/DegiskenlerVeVeriTipleri/Program.cs
@@ -4,81 +4,28 @@ namespace DegiskenlerVeVeriTipleri
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("");
-            String degisken =" ";  
-            
-            byte b= 5;      //1 byte
-            sbyte c=5;      //1 byte
-            short s=5;      //2 byte
-            ushort us=5;    //2 byte
+        public static double solve(double meal_cost, int tip_percent, int tax_percent)
+    {
+        double toplam=0;
+        double tip=0;
+        double tax=0;
+        tip=(tip_percent*(meal_cost/100));
+        Console.WriteLine(tip);
+        tax=(tip_percent*(tax_percent/100));
+        Console.WriteLine(tax);
+        return meal_cost+tip+tax;
+    }
+        public static void Main(string[] args)
+    {
+        double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
 
-            Int16 i16= 2;   //2 byte
-            int i=2;        //4 byte
-            int i32=2;    //4 byte
+        int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
 
-            uint ui =2 ;      //4 byte 
-            long l=4;         //8 byte
-            ulong ul=4;       //8 byte
+        int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+        Console.WriteLine(solve(meal_cost, tip_percent, tax_percent));
 
-            float f=4;        //4 byte
-            double d=5;       //8 byte
-            decimal de= 5;    //16 byte
-
-            char ch='2';      //2 byte;
-            string str = "Serkan ";//sınırsız
-
-            DateTime dt= DateTime.Now; 
-            
-            System.Console.WriteLine(dt);
-
-            object o1="x";
-            object o2='y';
-            object o3 = 4;
-            object o4=4.1;
-
-            //string ifadeler
-
-            string str1 = string.Empty;
-            str1 = "Serkan YILMAZ";
-            string ad = "Serkan";
-            string soyad="YILMAZ";
-            string tamAd=ad+" "+soyad;
-            
-            //integer tanımlama şekilleri
-
-            int integer1 = 5;
-            int integer2=3;
-            int integer3 = integer1* integer2;
-
-            //boolean 
-
-            bool b1= 10<2;
-
-            //degisken donusumleri
-
-            string  str20 = "20";
-            int int20=20;
-            
-            string yeniDeger= str20+int20.ToString();
-            System.Console.WriteLine(yeniDeger);
-
-            int int21 = int20+Convert.ToInt32(str20);
-            System.Console.WriteLine(int21);
-
-            int int22= int20+int.Parse(str20);
-
-
-            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
-            System.Console.WriteLine(datetime);
-            
-            string datetime2 = DateTime.Now.ToString("dd/MM/yyyy");
-            System.Console.WriteLine(datetime2);
-
-            string datetime3 = DateTime.Now.ToString("HH.MM");
-            System.Console.WriteLine(datetime3);
-        }
+        
+    }
 
 
     }
---
+++ CSharp/Operatorler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int a = 8;
+        a+=2;
+        Console.WriteLine(a);
+
+        a/=2;
+        Console.WriteLine(a);
+
+        bool isCompleted = true;
+        bool isSuccess = true;
+
+        if(isCompleted && isSuccess) {
+            Console.WriteLine("Operation succesfully completed");
+        }
+
+        a = 5;
+        int b = 2;
+
+        bool control = b < a;
+        Console.WriteLine(control);
+
+        a = 8;
+        b = 2;
+
+        int result = a*b;
+        Console.WriteLine(result);
+
+        result = a/b;
+        Console.WriteLine(result);
+
+        result = a % b;
+        Console.WriteLine(result);
+
+        double c = 5.0;
+        int f = 5;
+        double result3 = c+f;
+        Console.WriteLine(result3);
+        }
+    }
+}
